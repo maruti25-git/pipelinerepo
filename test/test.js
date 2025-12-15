116e636c8d8157e35490526f5cc4cc0c020bf422
marutimarathe <marutimarathe2003@gmail.com>
2025-12-15 14:03:00 +0000

    mm

--- test/test.js
+++ test/test.js
@@ -1,14 +1,11 @@
-              var request = require('supertest');
-              var app = require('../index.js');
-              
-              describe('GET /', function () {
-                after(function () {
-                  process.exit(0); // Ensure Mocha exits after tests
-                });
-              
-                it('respond with 404 page not found !!', function (done) {
-                  request(app)
-                    .get('/nonexistentpage')
-                    .expect(404, done);
-                });
-              });
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function () {
+  it('respond with 404 page not found !!', function (done) {
+    request(app)
+      .get('/nonexistentpage')
+      .expect(404, done);
+  });
+});
+
